91387040e10cf333f085e8abf1aadf7f5a07601d
Ricardo <ricardomg92@hotmail.com>
2022-03-15 22:00:09 +0100

    Add iteration method for fibonacci

---
+++ lib/fibonacci.rb
@@ -0,0 +1,15 @@
+def fibs(number)
+  iteration = 0
+  fib = []
+  while iteration < number
+    fib << 0 if iteration.zero?
+    fib << 1 if iteration.eql?(1)
+    fib << fib[iteration - 1] + fib[iteration - 2] if iteration > 1
+    iteration += 1
+  end
+  fib
+end
+
+p fibs(8)
+
+
